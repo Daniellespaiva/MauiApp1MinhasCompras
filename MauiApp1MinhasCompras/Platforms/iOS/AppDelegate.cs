9384e70fcd3c2c10c52e1a72521ccac648df4131
Danielle Paiva <daniellespaiva@outlook.com>
2025-03-03 16:28:17 -0300

    Adicionar arquivos de projeto.

---
+++ MauiApp1MinhasCompras/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace MauiApp1MinhasCompras;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
